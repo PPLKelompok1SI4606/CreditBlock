a4b989898ba252bf82e9eb8060e67cf878d73646
luhatfimhalaf <luhatfim.halaf1@gmail.com>
2025-04-11 06:44:49 +0700

    Commit Dashboard User dan Admin

--- kredit-blockchain/routes/web.php
+++ kredit-blockchain/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('/admin-dashboard', [AdminController::class, 'index'])->name('admin.dashboard'); // Daftar pengguna & pinjaman aktif
