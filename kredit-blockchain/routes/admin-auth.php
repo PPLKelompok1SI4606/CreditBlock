42cce55e443a23578970d0a1c9c9bd8cb00c4cec
yohanpratamaa <yohanpratamaa@gmail.com>
2025-04-11 22:23:23 +0700

    update

--- kredit-blockchain/routes/admin-auth.php
+++ kredit-blockchain/routes/admin-auth.php
@@ -4,20 +4,14 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
+Route::middleware('guest:admin')->group(function () {
 
-    // Register
-    Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
-    Route::post('register', [RegisteredUserController::class, 'store']);
-
-    // Login
     Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
 });
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth:admin')->group(function () {
 
-    // LogOut
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
