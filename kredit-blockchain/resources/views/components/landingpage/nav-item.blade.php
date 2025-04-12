406704d53f0801ed43a6e729c538de0d75eb2307
yohanpratamaa <yohanpratamaa@gmail.com>
2025-04-12 13:32:52 +0700

    update

--- kredit-blockchain/resources/views/components/landingpage/nav-item.blade.php
+++ kredit-blockchain/resources/views/components/landingpage/nav-item.blade.php
@@ -1,7 +1,7 @@
 <ul class="flex text-[#1167B1] font-semibold">
-    <li class="ml-10"><a href="#"> Home </a></li>
-    <li class="ml-5 mr-5"><a href="#"> Compare Lenders </a></li>
-    <li class="mr-5"><a href="#"> Saving Calculator </a></li>
-    <li class="mr-5"><a href="#"> Blog </a></li>
-    <li class="mr-5"><a href="#"> Contact Us </a></li>
+    <li class="ml-10 transition duration-300 ease-in-out hover:-translate-y-1"><a href="#"> Home </a></li>
+    <li class="ml-5 mr-5 transition duration-300 ease-in-out hover:-translate-y-1"><a href="#"> Compare Lenders </a></li>
+    <li class="mr-5 transition duration-300 ease-in-out hover:-translate-y-1"><a href="#"> Saving Calculator </a></li>
+    <li class="mr-5 transition duration-300 ease-in-out hover:-translate-y-1"><a href="#"> Blog </a></li>
+    <li class="mr-5 transition duration-300 ease-in-out hover:-translate-y-1"><a href="#"> Contact Us </a></li>
 </ul>
