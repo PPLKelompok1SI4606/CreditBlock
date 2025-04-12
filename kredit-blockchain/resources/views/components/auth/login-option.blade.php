03054545f793c964bf4b2bc25f19caa9179eb418
yohanpratamaa <yohanpratamaa@gmail.com>
2025-04-12 09:25:11 +0700

    update

--- kredit-blockchain/resources/views/components/auth/login-option.blade.php
+++ kredit-blockchain/resources/views/components/auth/login-option.blade.php
@@ -1,4 +1,4 @@
-<div class="inline-flex border border-gray-200 items-center justify-center w-[200px] h-[60px] bg-gray-100 rounded-full transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
+<div class="inline-flex border border-gray-200 items-center justify-center w-[200px] h-[60px] bg-gray-100 rounded-full transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105">
     {{ $slot }}
 </div>
 
