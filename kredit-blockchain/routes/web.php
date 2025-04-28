050e4a33b8787b41c445fc1792e25908a3adcf27
yohanpratamaa <yohanpratamaa@gmail.com>
2025-04-28 15:41:10 +0700

    update

--- kredit-blockchain/routes/web.php
+++ kredit-blockchain/routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoanCalculatorController;
+use App\Http\Controllers\LoanApplicationController;
 
 Route::get('/', function () {
     return view('landingpage');
@@ -27,3 +30,18 @@ Route::post('/calculate-loan', [LoanCalculatorController::class, 'calculate'])->
 
 require __DIR__.'/auth.php';
 require __DIR__.'/admin-auth.php';
+
+Route::middleware('auth')->group(function () {
+    // Dashboard untuk pengguna yang sudah login
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    // Rute untuk pembayaran
+    Route::get('/payments/create', [PaymentController::class, 'create'])->name('payments.create'); // Form pembayaran
+    Route::post('/payments/store', [PaymentController::class, 'store'])->name('payments.store'); // Proses pembayaran
+    Route::get('/payments/history', [PaymentController::class, 'history'])->name('payments.history'); // Riwayat pembayaran
+
+    // Rute untuk memeriksa status pinjaman
+    Route::get('/payments/check-loan-status', [LoanApplicationController::class, 'checkLoanStatus'])->name('payments.check-loan-status');
+
+    Route::get('/payments/all-history', [PaymentController::class, 'allHistory'])->name('payments.all-history');
+});
