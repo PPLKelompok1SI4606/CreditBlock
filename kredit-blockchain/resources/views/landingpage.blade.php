965864e9a0f4011a7efe99f23a256d6c0f5f278f
amiertia <ertiaami@gmail.com>
2025-06-02 23:27:41 +0700

    Update judul calculate

--- kredit-blockchain/resources/views/landingpage.blade.php
+++ kredit-blockchain/resources/views/landingpage.blade.php
@@ -213,7 +213,7 @@
     {{-- Loan Calculator --}}
     <section class="flex flex-col items-center w-full h-full px-[100px] py-[60px] text-center relative" id="loan-calculator">
     <h1 class="text-[#2A9DF4] font-semibold tracking-widest text-lg hidden-animated">LOAN CALCULATOR</h1>
-    <h1 class="text-[40px] w-[900px] tracking-wide font-bold text-[#1167B1] hidden-animated">Hitung Cicilanmu Sekarang & Temukan Cara Lebih Ringan untuk Membayar!</h1>
+    <h1 class="text-[40px] w-[900px] tracking-wide font-bold text-[#1167B1] hidden-animated">Hitung Sekarang, Bayar Lebih Mudah, Hidup Lebih Tenang!</h1>
 
     <div class="flex flex-col items-center px-10 py-10 bg-white mt-10 rounded-xl border h-full border-gray-300 shadow-blue-300 shadow-xl">
         <x-landingpage.loan-calculator></x-landingpage.loan-calculator>
